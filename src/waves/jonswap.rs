d7095a6ac217d5b718901eccdc369706d4a16882
michael.morristhomas <michael.morristhomas@me.com>
2024-08-02 11:14:11 +0800

    feat: implement `SpectralCommon` in jonswap

--- src/waves/jonswap.rs
+++ src/waves/jonswap.rs
@@ -2,12 +2,16 @@ use ndarray::{arr1, Array1, ArrayView1};
 use std::collections::HashMap;
 use std::f64::consts::PI;
 
+use crate::prelude::*;
+
 use crate::core::constants::GRAVITY;
 use crate::core::integration::trapz;
 use crate::core::interpolation;
 use crate::spectrums::{Spectrum1D, Spectrum2D};
 use crate::waves::bretschneider;
 use crate::waves::spreading::Spreading;
+
+use crate::waves::SpectralCommon;
 
 #[derive(Clone)]
 pub struct Jonswap {
@@ -235,6 +239,179 @@ pub fn convert_tp_to_t1(tp: f64, gamma: f64) -> f64 {
 
 pub fn convert_t1_to_tp(t1: f64, gamma: f64) -> f64 {
     t1 / (0.7303 + 0.04936 * gamma - 0.006556 * gamma.powf(2f64) + 0.0003610 * gamma.powf(3f64))
+}
+
+/// The original JONSWAP spectral formulation from the joint North Sea wave project.
+///
+/// # Description
+///
+/// Implementation of the original JONSWAP spectral formulation as
+/// described in Hasselmann et al. (1973)[^1]. The form of which is
+/// dependent upon the wind speed $U_{10}$ defined at 10$~\mathrm{m}$ above the
+/// sea surface and the fetch length $x$ defined in $\mathrm{m}$. The
+/// one-dimensional energy density, as a function of $\omega$, with five
+/// free parameters $\omega_p$, $\alpha$, $\gamma$, $\sigma_a$ and
+/// $\sigma_b$ is written
+/// (Hasselmann *et. al.*[^1], Eq. 2.4.1):
+/// $$
+/// S(\omega) = \alpha g^{2} \omega^{-5}
+/// \exp \left[-\frac{5}{4} \left(\frac{\omega_p}{\omega}\right)^4 \right]
+/// \gamma^G,
+/// $$
+/// $$
+/// G = \exp \left[-\frac{(\omega-\omega_p)^2}{2 \sigma^{2} \omega_{p}^{2}}\right],
+/// $$
+/// The free parameters are defined as:
+/// $$
+/// \alpha = 0.0076 (U_{10}^{2} / gF)^{0.22},
+/// $$
+/// $$
+/// \omega_{p} = 2 \pi 3.5 (g^{2} / U_{10}F)^{1/3},
+/// $$
+/// $$
+/// \gamma=3.3,
+/// $$
+/// and
+/// $$
+/// \sigma =
+/// \begin{cases}
+/// 0.07,&\quad \omega \le \omega_p,\\\\
+/// 0.09,&\quad \omega >   \omega_p.\\
+/// \end{cases}
+/// $$
+///
+/// Where we have the following definitions:
+/// - $U_{10}$ is the wind speed at a height of $10~\mathrm{m}$ above the free-surface ($\mathrm{m/s}$)
+/// - $F$ is the fetch length measured in $\mathrm{m}$
+/// - $\omega_p$ is the peak spectral wave period (s)
+/// - $\gamma$ denotes the peak shape parameter  (-)
+/// - $g$ is the gravitational acceleration constant ($\mathrm{m^2/s}$)
+///
+/// The significant wave height $H_s$ can be recovered under the assumption of a narrow-banded process[^2]:
+/// $$
+/// H_{s} \equiv 0.0163 X^{0.5} U_{10}
+/// $$
+/// where $X$ is the fetch length in $\mathrm{km}$.
+///
+/// # Fields
+///
+/// * `u` - wind speed at 10$\mathrm{~m} above the sea surface in $\mathrm{m/s}$
+/// * `x` - fetch length in $\mathrm{m}$
+/// * `omega` - frequency space in $\mathrm{rad/s}$
+///
+/// # Methods
+///
+/// * `hs` - significant wave height in $\mathrm{m}$
+/// * `tp` - peak spectral wave period in $\mathrm{s}$
+/// * `wp` - peak spectral wave frequency in $\mathrm{rad/s}$
+/// * `energy` - the energy density in $\mathrm{m^2 s/rad}$
+/// * `abs_error` - calculate the absolute error between the
+/// significant wave height and the corresponding sigmificant wave
+/// height calculated through the integrated energy density
+/// * `to_spec1d` - convert the one dimensional spectrum to a `Spectrum1` Type
+/// * `to_spec2d` - convert the one-dimensional spectrum to a `Spectrum2` Type for a given `Spreading` Type
+///
+/// # Default values
+///
+/// * `u` - 10.0 m/s
+/// * `x` - 1000.0 m
+/// * `omega` - 512 linearly spaced points between 0.1 and $\pi$ rad/s
+///
+/// # Example
+///
+/// ```
+/// use oceanwaves::waves::Jonswap1973;
+///
+/// let mut jonswap = Jonswap1973::default();
+/// let hs = jonswap.hs().unwrap();
+/// let tp = jonswap.tp().unwrap();
+/// let wp = jonswap.wp().unwrap();
+/// let energy = jonswap.energy();
+/// let abs_error = jonswap.abs_error().unwrap();
+/// ```
+///
+/// # References
+///
+/// [^1]: Hasselmann, K. *et al.* Measurements of wind-wave growth and
+/// swell decay during the joint North Sea wave project
+/// (JONSWAP). *Deutschen Hydrographischen Zeitschrift* **12**,
+/// (1973).
+/// [^2]: Hasselmann, K., Sell, W., Ross, D. B. & M$\text{\\\"{u}}$ller, P. A
+/// Parametric Wave Prediction Model. *Journal of Physical
+/// Oceanography* **6**, 200 228 (1976).
+///
+pub struct Jonswap1973 {
+    pub u: f64,
+    pub x: f64,
+    pub omega: Array1<f64>,
+}
+
+fn energy_jonswap1973(omega: ArrayView1<f64>, u: f64, x: f64) -> Array1<f64> {
+    // default constants as per JONSWAP 1973
+    let gamma: f64 = 3.3;
+    let sigma_a: f64 = 0.07;
+    let sigma_b: f64 = 0.09;
+
+    let alpha = 0.076 * (u.powi(2) / (x * GRAVITY));
+    let omega_p = TWO_PI * 3.5 * (GRAVITY.powi(2) / (u * x)).powf(1. / 3.);
+
+    omega
+        .iter()
+        .map(|w| {
+            let sigma = match w {
+                w if w <= &omega_p => sigma_a,
+                _ => sigma_b,
+            };
+            let shape_factor = (-0.5 * ((w - omega_p) / (sigma * omega_p)).powi(2)).exp();
+            let a = alpha * GRAVITY.powi(2) * w.powi(-5) * (-1.25 * (omega_p / w).powi(4)).exp();
+            a * gamma.powf(shape_factor)
+        })
+        .collect()
+}
+
+impl Default for Jonswap1973 {
+    fn default() -> Self {
+        Jonswap1973 {
+            u: 10.0,
+            x: 1000.0,
+            omega: Array1::<f64>::linspace(0.1, PI, 512),
+        }
+    }
+}
+
+impl SpectralCommon for Jonswap1973 {
+    fn omega(&self) -> &Array1<f64> {
+        &self.omega
+    }
+
+    fn hs(&self) -> Result<f64, String> {
+        Ok(0.0163 * (self.x / 1000.).sqrt() * self.u)
+    }
+
+    fn tp(&self) -> Result<f64, String> {
+        Ok(TWO_PI / self.wp()?)
+    }
+
+    fn wp(&self) -> Result<f64, String> {
+        Ok(TWO_PI * 3.5 * (GRAVITY.powi(2) / (self.u * self.x)).powf(1. / 3.))
+    }
+
+    fn energy(&self) -> Array1<f64> {
+        energy_jonswap1973(self.omega.view(), self.u, self.x)
+    }
+
+    fn abs_error(&self) -> Result<f64, String> {
+        let area = trapz(self.energy().view(), self.omega().view());
+        Ok((self.hs()? - 4.0 * area.sqrt()).abs())
+    }
+
+    fn to_spec1d(&self) -> Spectrum1D {
+        Spectrum1D::new(self.omega().to_owned(), self.energy().to_owned()).to_owned()
+    }
+
+    fn to_spec2d(&self, spreading: &Spreading) -> Spectrum2D {
+        Spectrum2D::from_spec1d(&self.to_spec1d(), spreading)
+    }
 }
 
 #[cfg(test)]
