38b204e341fcd722feba8f096119babc7b1cfde2
michael.morristhomas <michael.morristhomas@me.com>
2024-08-02 11:16:04 +0800

    feat: initial implementation of the Ochi spectrum

--- src/waves/mod.rs
+++ src/waves/mod.rs
@@ -1,22 +1,49 @@
-//! ocean wave energy density spectral formulation
+//! Ocean wave energy density spectral formulations
 //!
 //! # Summary
 //!
-//! The `waves` module provides ocean wave energy density spectral formulations that are commonly used
-//! in oceanography, meteorology, and engineering design. The module provides the following spectral formulations:
+//! The `waves` module provides ocean wave energy density spectral
+//! formulations that are commonly used in oceanography, meteorology,
+//! and engineering design. The module provides the following spectral
+//! formulations:
 //!
-//! * `bretschneider` - Bretschneider wave spectrum
+//! * `Phillips1958`- the original formulation by Phillips[^1] that
+//! captures the high-frequency behaviour of the spectral energy for
+//! wind-generated ocean waves
+//! * `Phillips` - Phillips' formulation re-cast into wave spectrum[^2] as a function of wind speed $U$  
+//! * `bretschneider` - the Bretschneider[^3] wave spectrum
 //! * `gaussian` - Gaussian wave spectrum
 //! * `jonswap` - JONSWAP wave spectrum
 //! * `ochi` - Ochi-Hubble wave spectrum
 //!
-//! In addition, the module also provides commonly used spreading functions used to generate two-dimensional
-//! wave spectra from one-dimensional wave spectra. These are:
+//! In addition, the module also provides spreading functions commonly
+//! used to generate two-dimensional wave spectra (short-crested
+//! waves) from one-dimensional wave spectra (long-crested). These
+//! are:
 //!
-//! *
+//! # References
+//!
+//! [^1]: Phillips, O. M. (1958). The equilibrium range in the
+//! spectrum of wind-generated waves. *Journal of Fluid Mechanics*,
+//! 4(4): 426-434.
+//! [^2]: M. J. Tucker and E. G. Pitt, *Waves in Ocean Engineering*, Volume 5. Elsevier Science, 2001.
+//! [^3]: Bretschneider, C. L. (1958). Wave Variability and Wave
+//! Spectra for Wind-Generated Gravity Waves. Technical Report 1,
+//! U.S. Army Coastal Engineering Research Center, Fort Belvoir, VA.
+//!
 
-pub mod bretschneider;
+mod bretschneider;
+mod common;
 pub mod gaussian;
 pub mod jonswap;
-pub mod ochi;
+mod ochi;
+mod phillips;
+mod pierson_moskowitz;
 pub mod spreading;
+
+pub use bretschneider::Bretschneider;
+pub use common::SpectralCommon;
+pub use jonswap::Jonswap1973;
+pub use ochi::OchiHubble;
+pub use phillips::{Phillips, Phillips1958};
+pub use pierson_moskowitz::PiersonMoskowitz;
--- src/waves/ochi.rs
+++ src/waves/ochi.rs
@@ -1,17 +1,40 @@
-/// The Ochi-Hubble [1] energy density spectrum is defined [1]:
-///
-/// ```latex
-/// ```
-///
-use ndarray::Array1;
-use std::f64::consts::PI;
+//! The Ochi-Hubble energy density spectru:
+//!
+//!
+//!
+use crate::prelude::*;
+use ndarray::{Array1, ArrayView1};
+use special::Gamma;
+
+use crate::waves::common::SpectralCommon;
 
 const N_FREQ: usize = 256;
 
 pub struct OchiHubble {
     pub hs: [f64; 2],
     pub tp: [f64; 2],
+    pub lambda: [f64; 2],
     pub omega: Array1<f64>,
+}
+
+pub fn energy(omega: ArrayView1<f64>, hs: [f64; 2], tp: [f64; 2], lambda: [f64; 2]) -> Array1<f64> {
+    let a = |w: f64, l: f64| w.powf(-4. * (l + 0.25)) * (-w.powf(-4.) * (l + 0.25)).exp();
+    let b = |l: f64| 4. * (l + 0.25).powf(l) / Gamma::gamma(l);
+    let c = |h: f64, t: f64| h.powf(2.) * t / (32. * PI);
+
+    let energy: Array1<f64> = omega
+        .iter()
+        .map(|w_i| {
+            let w_n = [w_i * tp[0] / TWO_PI, w_i * tp[1] / TWO_PI];
+            let mut sum = 0.0;
+            for i in 0..2 {
+                sum += a(w_n[i], lambda[i]) * b(lambda[i]) * c(hs[i], tp[i]);
+            }
+            sum
+        })
+        .collect();
+
+    energy
 }
 
 impl Default for OchiHubble {
@@ -19,7 +42,104 @@ impl Default for OchiHubble {
         Self {
             hs: [1.0, 0.5],
             tp: [20.0, 8.0],
+            lambda: [0.5, 0.5],
             omega: Array1::<f64>::linspace(0.1, PI, N_FREQ),
         }
+    }
+}
+
+impl SpectralCommon for OchiHubble {
+    /// rms significant wave height approximation
+    fn hs(&self) -> Result<f64, String> {
+        match self.hs.len() {
+            2 => Ok((self.hs[0].powf(2.) + self.hs[1].powf(2.)).sqrt()),
+            _ => Err("hs is not length 2".to_string()),
+        }
+    }
+
+    /// rms peak spectral wave period approximation
+    fn tp(&self) -> Result<f64, String> {
+        match self.tp.len() {
+            2 => Ok((self.tp[0].powf(2.) + self.tp[1].powf(2.)).sqrt()),
+            _ => Err("tp is not length 2".to_string()),
+        }
+    }
+
+    fn omega(&self) -> &Array1<f64> {
+        &self.omega
+    }
+
+    fn energy(&self) -> Array1<f64> {
+        energy(self.omega.view(), self.hs, self.tp, self.lambda)
+    }
+}
+
+impl OchiHubble {
+    /// Create a new Ochi-Hubble spectrum
+    pub fn new(hs: [f64; 2], tp: [f64; 2], lambda: [f64; 2]) -> Self {
+        Self {
+            hs,
+            tp,
+            lambda,
+            ..Default::default()
+        }
+    }
+
+    /// Set the significant wave height for each component
+    pub fn set_hs(&mut self, hs: [f64; 2]) -> &mut Self {
+        self.hs = hs;
+        self
+    }
+
+    /// Set the peak spectral wave period for each component
+    pub fn set_tp(&mut self, tp: [f64; 2]) -> &mut Self {
+        self.tp = tp;
+        self
+    }
+
+    /// Set the shape factor for each component
+    pub fn set_lambda(&mut self, lambda: [f64; 2]) -> &mut Self {
+        self.lambda = lambda;
+        self
+    }
+
+    /// Return the peak spectral wave frequency for each component
+    pub fn get_wp(&self) -> Result<[f64; 2], String> {
+        match self.tp.len() {
+            2 => Ok([self.tp[0] / TWO_PI, self.tp[1] / TWO_PI]),
+            _ => Err("tp is not length 2".to_string()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ochi_hubble() {
+        let ochi = OchiHubble::default();
+        let energy = ochi.energy();
+        assert_eq!(energy.len(), N_FREQ);
+    }
+
+    #[test]
+    fn test_ochi_hubble_hs() {
+        let ochi = OchiHubble::default();
+        let hs = ochi.hs().unwrap();
+
+        assert_eq!(ochi.hs[0], 1.0_f64);
+        assert_eq!(ochi.hs[1], 0.5_f64);
+        assert_eq!(hs, (1.25_f64).sqrt());
+    }
+
+    #[test]
+    fn test_ochi_hubble_tp() {
+        let ochi = OchiHubble::default();
+        let tp = ochi.tp().unwrap();
+
+        assert_eq!(ochi.tp[0], 20.0_f64);
+        assert_eq!(ochi.tp[1], 8.0_f64);
+        assert_eq!(tp, (20.0_f64.powf(2.) + 8.0_f64.powf(2.)).sqrt());
     }
 }
